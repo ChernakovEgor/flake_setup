e6fad306ea80f8afb0c6c4ef029aed9c27a9f88b
Chernakov Egor <chernakov.eg@gmail.com>
2025-04-29 13:58:23 +0300

    update

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "egor's home config";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
