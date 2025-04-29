cca75d23ac9ebfa737fdcf9033e9b3268326cf50
Chernakov Egor <chernakov.eg@gmail.com>
2025-04-29 13:36:45 +0300

    add flake.lock

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "egor's home config";
 
-  input = {
+  inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
