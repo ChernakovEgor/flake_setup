02c43b60aee77d83c5291d53df5b2fb656e7e2b6
Chernakov Egor <chernakov.eg@gmail.com>
2025-04-30 17:04:27 +0300

    update

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -11,13 +11,34 @@
 
   # custom options for programs
   programs = {
+    jq.enable = true;
+    home-manager.enable = true;
+
     git = {
       enable = true;
       userName = "Chernakov Egor";
       userEmail = "chernakov.eg@gmail.com";
+      defaultBranch = "main";
     };
+    
+    zsh = {
+      enable = true;
+      enableAutosuggestions = true;
+      enableSyntaxHighlighting = true;
+    # shellAliases = {
+    #   ll = "ls -l";
+    #   cd = "z";
+    # };
 
-    jq.enable = true;
-    home-manager.enable = true;
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+          "git"
+          # "zsh-autosuggestions"
+          "powerlevel10k"
+      ];
+
+      theme = "robbyrussell";
+    };
   };
 }
